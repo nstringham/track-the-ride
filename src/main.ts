a2f0ff9e84216cd8caa3f1ae82a14b7a4fadecd1
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2026-04-16 21:37:28 -0400

    feat: ✨ add basic map with leaflet

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
 import "./style.css";
+import "leaflet/dist/leaflet.css";
+import L, { LatLng } from "leaflet";
+
+const BTC = new LatLng(42.278425133686646, -83.74699558741413);
+
+var map = L.map("map").setView(BTC, 13);
+
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
