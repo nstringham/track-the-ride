ad2e306b85166723966b849626ba833d98b0e8c2
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2026-04-17 00:41:13 -0400

    feat: ✨ add data to markers

--- src/map.ts
+++ src/map.ts
@@ -14,7 +14,28 @@ L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
 const group = L.layerGroup().addTo(map);
 
 export function setBusLocations(locations: BusLocation[]) {
+  group.clearLayers();
+
   for (const location of locations) {
-    L.circleMarker([location.latitude, location.longitude]).addTo(group);
+    const marker = L.circleMarker([location.latitude, location.longitude]).addTo(group);
+
+    const timestamp = Temporal.Instant.from(location.timestamp);
+    const age = Temporal.Now.instant().since(timestamp);
+    marker.bindPopup(`Bus #${location.bus_id}<br>${timeFormatter.format(timestamp)}`);
+
+    const hue = parseInt(location.bus_id) % 360;
+    const chroma = 0.15 * (1 - age.total("minutes") / 60);
+    const color = `oklch(0.6 ${chroma} ${hue})`;
+
+    marker.setStyle({
+      radius: 12,
+      fillColor: color,
+      color: color,
+      weight: 1,
+      opacity: 1,
+      fillOpacity: 0.5,
+    });
   }
 }
+
+const timeFormatter = new Intl.DateTimeFormat("en-US", { timeStyle: "short" });
