1e61a59e60f50ecfde77dcd32c050c1c38245c76
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2026-04-18 20:38:30 -0400

    feat: ✨ adjust colors to be slightly more vibrant

--- src/color.ts
+++ src/color.ts
@@ -1,5 +1,8 @@
 const busHues: { [busId: string]: number } = {};
 
+const lightness = 2 / 3;
+const maxChroma = 0.175;
+
 export function getBusColor(busId: string, timestamp: Temporal.Instant): string {
   if (!(busId in busHues)) {
     busHues[busId] = (Object.keys(busHues).length * 137.5) % 360;
@@ -9,7 +12,7 @@ export function getBusColor(busId: string, timestamp: Temporal.Instant): string 
 
   const ageMinutes = Temporal.Now.instant().since(timestamp).total("minutes");
 
-  const chroma = 0.15 * (1 - ageMinutes / 60);
+  const chroma = maxChroma * (1 - ageMinutes / 60);
 
-  return `oklch(0.6 ${chroma} ${hue})`;
+  return `oklch(${lightness} ${chroma} ${hue})`;
 }
